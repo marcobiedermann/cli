5d3392e86797af4fd07ee0e44f5279947068f517
Marco Biedermann <marco.biedermann@gmx.de>
2020-09-16 22:09:52 +0200

    feat: Content
    Add content

--- lib/components/App/index.jsx
+++ lib/components/App/index.jsx
@@ -1,6 +1,62 @@
-const { Text } = require('ink');
+const { Box, Text } = require('ink');
+const BigText = require('ink-big-text');
+const Gradient = require('ink-gradient');
+const SelectInput = require('ink-select-input').default;
+const open = require('open');
 const React = require('react');
 
-const App = () => <Text>Hello, world</Text>;
+function createItems(items) {
+  return items.map((item) => ({
+    ...item,
+    key: item.url || item.label,
+  }));
+}
+
+const items = createItems([
+  {
+    label: '🌎 Website',
+    url: 'https://marcobiedermann.com',
+  },
+  {
+    label: '💻 GitHub',
+    url: 'https://github.com/marcobiedermann',
+  },
+  {
+    label: '🐦 Twitter',
+    url: 'https://twitter.com/intent/user?screen_name=BiedermannMarco',
+  },
+  {
+    label: '🧑‍💼 LinkedIn',
+    url: 'https://www.linkedin.com/in/biedermannmarco',
+  },
+  {
+    label: '🚪 Quit',
+    action() {
+      process.exit();
+    },
+  },
+]);
+
+function handleSelect(item) {
+  if (item.url) {
+    open(item.url);
+  }
+
+  if (item.action) {
+    item.action();
+  }
+}
+
+const App = () => (
+  <Box flexDirection="column">
+    <Gradient name="pastel">
+      <BigText text="Marco Biedermann" font="tiny" />
+    </Gradient>
+    <Box margin={1}>
+      <Text>Full-Stack JavaScript Engineer</Text>
+    </Box>
+    <SelectInput items={items} onSelect={handleSelect} />
+  </Box>
+);
 
 module.exports = App;
